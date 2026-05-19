e28236a2ba5c8af91cd43ef13b24c76bf48e1e28
Hus-GPT <bnjwygh@gmail.com>
2026-05-19 03:14:50 +0300

    Update BOT_TOKEN in config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-# مِلَفّ الإِعْدَادَات الرَّئِيْسِيَّة لِلْمَشْرُوع
-BOT_TOKEN = "8711628726:AAH6deQFqOLoVZHBVacIDablXW9TUYBbBDI"
-GROUP_DATABASE_ID = None 
+BOT_TOKEN = "7771735537:AAEy5nfjOflwlBum2yUARm0XC2ptCSnK_to"
+GROUP_DATABASE_ID = None
